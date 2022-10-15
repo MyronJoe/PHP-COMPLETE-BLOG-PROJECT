0cb26fbcbc1316f99b33837aba8d722c15a5d544
Myron <josephonyekachi77@gmail.com>
2022-10-14 19:44:08 -0800

    Created the Html and Css files

---
+++ index.php
@@ -0,0 +1,441 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Myron Blog</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
+
+<body>
+    <div class="containerr">
+
+        <div class="hero">
+            <div class="logo">
+                Myron Blog
+            </div>
+            <nav>
+                <div>
+                    <div class="cancel-icon">
+                        <span class="fas fa-times"></span>
+                    </div>
+                    <div class="menu-icon">
+                        <span class="fas fa-bars"></span>
+                    </div>
+                </div>
+                <div class="search-icon">
+                    <!-- <span class="fas fa-search"></span> -->
+                </div>
+                <div class="nav-items">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blogs</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Feedback</a></li>
+                </div>
+                <div class="user_handel">
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#"><i style="margin-right: 5px;" class="fas fa-user"></i>Kachi Joe<i
+                                style="margin-left: 5px;" class="fas fa-angle-down"></i></a></li>
+                </div>
+
+
+            </nav>
+            <form action="#" class="nav_form mobile_nav">
+                <input type="search" class="search-data" placeholder="Search" required>
+                <button type="submit" class="fas fa-search"></button>
+            </form>
+
+        </div>
+    </div>
+
+
+    <div class="content">
+
+        <section>
+            <!-------------------------------- Left side ------------------------------------>
+            <div class="left">
+                <!-------------------------------- Music section ------------------------------------>
+                <div class="header_line">
+                    <div class="header_title">
+                        <p><span>Lastest Song</span></p>
+                    </div>
+                </div>
+
+                <div class="Music">
+
+                    <div class="card">
+                        <div class="img_sec">
+
+                            <div class="topic">
+                                <p><a href="#">Entertainment</a></p>
+                            </div>
+                            <a href="#"><img src="images/images(56).jpg" alt="NEWS"></a>
+                        </div>
+                        <div class="title_sec">
+                            <a href="#">
+                                <h2 class="title">This is the title of this post as you know</h2>
+                            </a>
+
+                            <a href="#">
+                                <p class="post_content">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Quidem quas ut, veritatis
+                                    placeat odio earum!
+                                </p>
+                            </a>
+                            <small>BY <a href="#"><span style="color:red; margin-right: 10px;">JOE KACHI</span></a> Jul
+                                2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+
+
+
+                    <div class="more">
+                        <a href="">More Songs</a>
+                    </div>
+                </div>
+                <!-------------------------------- Video Section ------------------------------------>
+                <div class="header_line">
+                    <div class="header_title">
+                        <p><span>Lastest Video</span></p>
+                    </div>
+                </div>
+                <div class="Video">
+
+                    <div class="card">
+
+                        <div class="img_sec">
+                            <a href="#">
+                                <div class="Video_tag">
+
+                                    <p><span class="fas fa-play"></p>
+                                </div>
+                                <img src="images/NACOMES.jpeg" alt="NEWS">
+                            </a>
+                        </div>
+
+                        <div class="title_sec">
+                            <a href="#">
+                                <h2 class="title">This is the title of this post</h2>
+                            </a>
+
+                            <a href="#">
+                                <p class="post_content">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Quidem quas ut, veritatis
+                                    placeat odio earum!
+                                </p>
+                            </a>
+                            <small>BY <a href="#"><span style="color:red; margin-right: 10px;">JOE KACHI</span></a> Jul
+                                2, 2022 | 13:22</small>
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="more">
+                        <a href="">More Videos</a>
+                    </div>
+                </div>
+
+
+                <!-------------------------------- News section ------------------------------------>
+                <div class="header_line">
+                    <div class="header_title">
+                        <p><span>Lastest News</span></p>
+                    </div>
+                </div>
+                <div class="NEWS">
+
+                    <div class="card">
+                        <div class="img_sec">
+
+                            <div class="topic">
+                                <p><a href="#">Sports</a></p>
+                            </div>
+                            <a href="#"><img src="images/images(56).jpg" alt="NEWS"></a>
+                        </div>
+                        <div class="title_sec">
+                            <a href="#">
+                                <h2 class="title">This is the title of this post</h2>
+                            </a>
+
+                            <a href="#">
+                                <p class="post_content">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Quidem quas ut, veritatis
+                                    placeat odio earum!
+                                </p>
+                            </a>
+                            <small>BY <a href="#"><span style="color:red; margin-right: 10px;">JOE KACHI</span></a> Jul
+                                2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="more">
+                        <a href="">More News</a>
+                    </div>
+                </div>
+
+            </div>
+            <!-------------------------------- Right side ------------------------------------>
+            <div class="right">
+
+                <form action="#" class="nav_form desktop_nav">
+                    <input type="search" class="search-data" placeholder="Search" required>
+                    <button type="submit" class="fas fa-search"></button>
+                </form>
+
+
+                <div class="top_stories">
+                    <div class="header_line">
+                        <div class="header_title_side">
+                            <p><span>Top Stories</span></p>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">1</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/IMG-20220519-WA0023.jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title">This is the title of this post bcoz it is very important</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">2</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/images(56).jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">3</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/IMG-20220519-WA0023.jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">4</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/NACOMES.jpeg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">5</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/IMG-20220519-WA0023.jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="youtube" style="margin-bottom:10px;">
+                    <iframe width="100%" height="270" src="https://www.youtube.com/embed/byBI_GmWTCM"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                </div>
+
+
+                <div class="Comming soon">
+                    <div class="header_line">
+                        <div class="header_title_side">
+                            <p><span>Coming Soon</span></p>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">1</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/IMG-20220519-WA0023.jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title">This is the title of this post bcoz it is very important</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">2</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/images(56).jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">3</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/IMG-20220519-WA0023.jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">4</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/NACOMES.jpeg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                    <div class="mobile_card">
+                        <div class="mobile_img_sec">
+
+                            <div class="mtopic">
+                                <p style="font-size: 13px;">5</p>
+                            </div>
+                            <a href="#">
+                                <img src="images/IMG-20220519-WA0023.jpg" alt="NEWS">
+                            </a>
+                        </div>
+                        <div class="mobile_title_sec">
+                            <a href="#">
+                                <h4 class="title" style="margin-bottom: 5px;">This is the title of this post</h4>
+                            </a>
+
+                            <small>Jul 2, 2022 | 13:22</small>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="newslater">
+                    <h3 class="mb">Newsletter</h3>
+                    <p>Get weekly updates and entertainment news around Africa. Don't worry, we don't spam.</p>
+                    <form action="#" class="nav_form mt">
+                        <input type="search" class="search-data" placeholder="Search" required>
+                        <button type="submit" class="fas fa-search"></button>
+                    </form>
+                </div>
+
+
+            </div>
+        </section>
+
+    </div>
+
+    <div class="footer containerr">
+        <div class="social-icon">
+            <ul>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blogs</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Feedback</a></li>
+        </div>
+
+        <p>KorrectNaija &copy 2022</p>
+    </div>
+
+    <script src="assets/js/main.js"></script>
+    <script>
+
+    </script>
+</body>
+
+</html>

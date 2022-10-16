ae2b50ceac712b2ec2d0ea468da3073e77743b72
Myron <josephonyekachi77@gmail.com>
2022-10-16 13:39:27 -0800

    updated

--- Register.php
+++ Register.php
@@ -1,6 +1,13 @@
 <?php require_once 'path.php'; ?>
 <?php include(ROOT_PATH . "/app/includes/header.php") ?>
 
+<?php
+if (isset($_POST['register-btn'])) {
+    var_dump($_POST);
+    exit;
+}
+
+?>
     <div class="container">
 
         <div>
@@ -8,40 +15,39 @@
                 <div class="top">
                     <h4 class="m-0 text-uppercase font-weight-bold text-light">Register</h4>
                 </div>
-                <form method="POST" action="" novalidate class="form_sec" style="padding: 1.5em 2.5em 2.5em;">
 
+                <form method="POST" action="Register.php" novalidate class="form_sec" style="padding: 1.5em 2.5em 2.5em;">
                     <!-- <div class="form-error">
                         <p>Usernam needeed</p>
                         <p>Usernam needeed</p>
                     </div> -->
-
                     <div>
                         <label for="name">Name *</label>
-                        <input type="text" class="form-control" id="name">
+                        <input type="text" class="form-control" id="name" name="username">
                     </div>
 
 
                     <div>
                         <label for="email">Email *</label>
-                        <input type="email" class="form-control" id="email">
+                        <input type="email" class="form-control" id="email" name="email">
                     </div>
 
 
                     <div>
                         <label for="Password">Password *</label>
-                        <input type="password" class="form-control" id="Password">
+                        <input type="password" class="form-control" id="Password" name="password">
                     </div>
 
 
                     <div>
                         <label for="Confirm Password">Confirm Password *</label>
-                        <input type="password" class="form-control" id="Confirm Password">
+                        <input type="password" class="form-control" id="Confirm Password" name="passwordconfirm">
                     </div>
 
 
 
                     <div class="form-group mb-0">
-                        <input type="submit" value="sign up" class="btn">
+                        <input type="submit" name="register-btn" value="sign up" class="btn">
                     </div>
 
                     <div>

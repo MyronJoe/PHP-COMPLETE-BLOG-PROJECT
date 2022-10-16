89742377eb707c7a3dc9f28322d5429aa1ae37c0
Myron <josephonyekachi77@gmail.com>
2022-10-15 23:21:27 -0800

    UPDATED

--- app/database/db.php
+++ app/database/db.php
@@ -11,35 +11,59 @@
     }
 
     
-    //function that select from db and also checks conditions to select
-    function selectAll($table, $conditions = []){
+    // function that select from db and also checks conditions to select
+    // function selectAll($table, $conditions = []){
+    //     global $conn;
+    //     $sql = "SELECT * FROM $table";
+    //     if (empty($conditions)) {
+    //         $stmt = $conn->prepare($sql);
+    //         $stmt->execute();
+    //         $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    //         return $records;
+    //     }else{
+    //         $i = 0;
+            
+    //         foreach ($conditions as $key => $value) {
+
+    //             if ($i === 0) {
+    //                 $sql = $sql . "WHERE $key = '%', ?, '%'";
+    //             }else{
+    //                 $sql = $sql . "AND $key = '%', ?, '%'";
+    //             }
+    //             $i++;
+    //         }
+    //         $stmt = $conn->prepare($sql);
+    //         $values = array_values($conditions);
+    //         $types = str_repeat('s', count($values));
+    //         $stmt->bind_param($types, ...$values);
+    //         $stmt->execute();
+    //         $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    //         return $records;
+    //     }
+        
+    // }
+
+    function selectOne($table, $conditions){
         global $conn;
         $sql = "SELECT * FROM $table";
-        if (empty($conditions)) {
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-            return $records;
-        }else{
-            $i = 0;
-            
-            foreach ($conditions as $key => $value) {
+        $i = 0;
+        foreach ($conditions as $key => $value) {
 
-                if ($i === 0) {
-                    $sql = $sql . "WHERE $key= '%', ?, '%'";
-                }else{
-                    $sql = $sql . "AND $key= '%', ?, '%'";
-                }
-                $i++;
+            if ($i === 0) {
+                $sql = $sql . "WHERE $key = ?";
+            }else{
+                $sql = $sql . "AND $key = ?";
             }
-            $stmt = $conn->prepare($sql);
-            $values = array_values($conditions);
-            $types = str_repeat('s', count($values));
-            $stmt->bind_param($types, ...$values);
-            $stmt->execute();
-            $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-            return $records;
+            $i++;
         }
+        $stmt = $conn->prepare($sql);
+        $values = array_values($conditions);
+        $types = str_repeat('s', count($values));
+        $stmt->bind_param($types, ...$values);
+        $stmt->execute();
+        $records = $stmt->get_result()->fetch_assoc();
+        return $records;
+       
         
     }
 
@@ -47,6 +71,6 @@
         'email' => 'MyronJoe@gmail.com'
     ];
 
-    $users = selectAll('users', $conditions);
+    $users = selectOne('users', $conditions);
     dump($users)
 ?>

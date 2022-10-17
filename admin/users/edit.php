d1bede01e8d80d5ad0558cb0574df46013e53124
Myron <josephonyekachi77@gmail.com>
2022-10-17 15:29:37 -0800

    updated

---
+++ admin/users/edit.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../../path.php';
+include ROOT_PATH . '/admin/includes/adminheader.php';
+?>
+<section style="display: flex;">
+  <div class="lside">
+  <?php include ROOT_PATH . '/admin/includes/adminsidbar.php';?>
+  </div>
+
+  <div class="rside">
+    <div class="rside-top">
+      <a href="<?php echo BASE_URL . '/admin/users/create.php'; ?>" class="btn btn-primary mr-3">Add Users</a>
+      <a href="<?php echo BASE_URL . '/admin/users/index.php'; ?>" class="btn btn-primary">Manage Users</a>
+    </div>
+
+    <div class="adduser-sec">
+      <h2>Update Users</h2>
+
+      <form action="" method="" class="form">
+        <div class="form-group">
+          <label for="user">Name</label>
+          <input type="email" class="form-control" id="user" name="user">
+        </div>
+    
+        <div class="form-group">
+          <label for="description">Description</label>
+          <textarea class="form-control" id="description" rows="3" name=""></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary my-2">Update User</button>
+      </form>
+    </div>
+
+  </div>
+</section>
+
+</body>
+
+</html>
---
+++ admin/users/index.php
@@ -0,0 +1,57 @@
+<?php
+require_once '../../path.php';
+include ROOT_PATH . '/admin/includes/adminheader.php';
+?>
+<section style="display: flex;">
+    <div class="lside">
+        <?php include ROOT_PATH . '/admin/includes/adminsidbar.php'; ?>
+    </div>
+
+    <div class="rside">
+        <div class="rside-top">
+            <a href="<?php echo BASE_URL . '/admin/users/create.php'; ?>" class="btn btn-primary mr-3">Add User</a>
+            <a href="<?php echo BASE_URL . '/admin/users/index.php'; ?>" class="btn btn-primary">Manage User</a>
+        </div>
+
+        <div class="adduser-sec">
+            <h2>Manage Users</h2>
+
+            <table class="table mt-3">
+                <thead>
+                    <tr>
+                        <th scope="col">S/N</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Action</th>
+                        <!-- <th scope="col">Handle</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>
+                        <a href="<?php echo BASE_URL . '/admin/users/edit.php'; ?>" class="btn btn-sm btn-primary mr-1">Edit</a>
+                            <a href="" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>
+                            <a href="<?php echo BASE_URL . '/admin/users/edit.php'; ?>" class="btn btn-sm btn-primary mr-1">Edit</a>
+                            <a href="" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+
+
+    </div>
+</section>
+
+</body>
+
+</html>

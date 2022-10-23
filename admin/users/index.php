0ad75ed97446b4a28c068e244f135af3c60acfde
Myron <josephonyekachi77@gmail.com>
2022-10-22 21:06:30 -0800

    created admin functionality

--- admin/users/index.php
+++ admin/users/index.php
@@ -6,27 +6,22 @@ include ROOT_PATH . '/admin/includes/adminheader.php';
     <div class="lside">
         <?php include ROOT_PATH . '/admin/includes/adminsidbar.php'; ?>
     </div>
-
     <div class="rside">
         <div class="rside-top">
             <a href="<?php echo BASE_URL . '/admin/users/create.php'; ?>" class="btn btn-primary mr-3">Add User</a>
             <a href="<?php echo BASE_URL . '/admin/users/index.php'; ?>" class="btn btn-primary">Manage User</a>
         </div>
-
         <div class="adduser-sec">
             <h2>Manage Users</h2>
-
             <div class="table">
                 <?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
             </div>
-
             <table class="table mt-3">
                 <thead>
                     <tr>
                         <th scope="col">S/N</th>
                         <th scope="col">Name</th>
                         <th scope="col">Action</th>
-                        <!-- <th scope="col">Handle</th> -->
                     </tr>
                 </thead>
                 <tbody>
@@ -48,11 +43,7 @@ include ROOT_PATH . '/admin/includes/adminheader.php';
                     </tr>
                 </tbody>
             </table>
-
-
         </div>
-
-
     </div>
 </section>
 

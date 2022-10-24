24e5492bf8630070f782827f5d0e2c66f36ea30f
Myron <josephonyekachi77@gmail.com>
2022-10-23 23:23:51 -0800

    updated

--- admin/includes/adminsidbar.php
+++ admin/includes/adminsidbar.php
@@ -4,7 +4,9 @@
     <i class="fas fa-times" id="cancel"></i>
 </label>
 <div class="sidebar1">
-    <header>Dashboard</header>
+    <header>
+        <a href="<?php echo BASE_URL . '/admin/dashboard.php'; ?>">Dashboard</a>
+    </header>
     <ul>
         <li><a href="<?php echo BASE_URL . '/admin/posts/index.php'; ?>"><i class="fas fa-qrcode"></i>Manage Post</a></li>
         <li><a href="<?php echo BASE_URL . '/admin/users/index.php'; ?>"><i class="fas fa-calendar-week"></i>Manage User</a></li>

6c4cae74e3efa42e996285e9e57dacb88cbe97e7
Myron <josephonyekachi77@gmail.com>
2022-10-25 20:41:31 -0800

    updated

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,9 @@
+<link rel="stylesheet" href="../assets/css/admin.css">
 <?php
 require_once '../path.php';
 include ROOT_PATH . '/admin/includes/adminheader.php';
 include(ROOT_PATH . "/app/controllers/users.php");
+// include(ROOT_PATH . "/assets/css/admin.css");
 ?>
 <section style="display: flex;">
   <div class="lside">

b7a33c30123e6cf70f8716492e7059c780f83903
Myron <josephonyekachi77@gmail.com>
2022-10-27 22:28:22 -0800

    updated

---
+++ app/helpers/middlewear.php
@@ -0,0 +1,31 @@
+<?php 
+
+function usersOnly($redirect = '/index.php'){
+    if (empty($_SESSION["id"])) {
+        $_SESSION['message'] = 'Login required';
+        $_SESSION['type'] = 'danger';
+        header('location: ' .BASE_URL . $redirect);
+        exit();
+    }
+}
+
+
+function adminOnly($redirect = '/index.php'){
+    if (empty($_SESSION["id"]) || empty($_SESSION["admin"])) {
+        $_SESSION['message'] = 'You are not authorized';
+        $_SESSION['type'] = 'danger';
+        header('location: ' .BASE_URL . $redirect);
+        exit();
+    }
+}
+
+
+function guestOnly($redirect = '/index.php'){
+    if (empty($_SESSION["id"])) {
+        header('location: ' .BASE_URL . $redirect);
+        exit();
+    }
+}
+
+
+?>

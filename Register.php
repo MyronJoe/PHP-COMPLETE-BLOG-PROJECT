489bae10b04b4161ee095979145435851f555aab
Myron <josephonyekachi77@gmail.com>
2022-11-13 20:15:33 -0800

    updated

--- Register.php
+++ Register.php
@@ -1,58 +1,58 @@
-<?php require_once 'path.php'; 
+<?php require_once 'path.php';
 include(ROOT_PATH . "/app/controllers/users.php");
 include(ROOT_PATH . "/app/includes/header.php");
 guestOnly();
 
 ?>
-    <div class="container">
+<div class="container">
 
-        <div>
-            <div class="form " style="height:90vh; margin-top:12em;">
-                <div class="top">
-                    <h4 class="m-0 text-uppercase font-weight-bold text-light">Register</h4>
+    <div>
+        <div class="form " style="height:90vh; margin-top:12em;">
+            <div class="top">
+                <h4 class="m-0 text-uppercase font-weight-bold text-light">Register</h4>
+            </div>
+
+            <form method="POST" action="Register.php" novalidate class="form_sec" style="padding: 1.5em 2.5em 2.5em;">
+
+                <?php include(ROOT_PATH . "/app/helpers/formerrors.php") ?>
+
+                <div>
+                    <label for="name">Name *</label>
+                    <input type="text" class="form-control" value="<?php echo $username ?>" id="name" name="username">
                 </div>
 
-                <form method="POST" action="Register.php" novalidate class="form_sec" style="padding: 1.5em 2.5em 2.5em;">
 
-                    <?php include(ROOT_PATH . "/app/helpers/formerrors.php") ?>
-                    
-                    <div>
-                        <label for="name">Name *</label>
-                        <input type="text" class="form-control" value="<?php echo $username ?>" id="name" name="username">
-                    </div>
+                <div>
+                    <label for="email">Email *</label>
+                    <input type="email" class="form-control" value="<?php echo $email ?>" id="email" name="email">
+                </div>
 
 
-                    <div>
-                        <label for="email">Email *</label>
-                        <input type="email" class="form-control"  value="<?php echo $email ?>" id="email" name="email">
-                    </div>
+                <div>
+                    <label for="Password">Password *</label>
+                    <input type="password" class="form-control" value="<?php echo $password ?>" id="Password" name="password">
+                </div>
 
 
-                    <div>
-                        <label for="Password">Password *</label>
-                        <input type="password" class="form-control" value="<?php echo $password ?>" id="Password" name="password">
-                    </div>
-
-
-                    <div>
-                        <label for="Confirm Password">Confirm Password *</label>
-                        <input type="password" class="form-control" value="<?php echo $confirmpass ?>" id="Confirm Password" name="passwordconfirm">
-                    </div>
+                <div>
+                    <label for="Confirm Password">Confirm Password *</label>
+                    <input type="password" class="form-control" value="<?php echo $confirmpass ?>" id="Confirm Password" name="passwordconfirm">
+                </div>
 
 
 
-                    <div class="form-group mb-0">
-                        <input type="submit" name="register-btn" value="sign up" class="btn">
-                    </div>
+                <div class="form-group mb-0">
+                    <input type="submit" name="register-btn" value="sign up" class="btn">
+                </div>
 
-                    <div>
-                        <small style="font-size: 11px;">Already have an account? <a href="<?php echo BASE_URL . '/login.php'?>">Sing In</a></small>
-                    </div>
-                </form>
+                <div>
+                    <small style="font-size: 11px;">Already have an account? <a href="<?php echo BASE_URL . '/login.php' ?>">Sing In</a></small>
+                </div>
+            </form>
 
-            </div>
         </div>
-
     </div>
 
-    <?php include(ROOT_PATH . "/app/includes/footer.php") ?> 
+</div>
+
+<?php include(ROOT_PATH . "/app/includes/footer.php") ?>

ee26681884431df102fda30e151ea14cad141e3d
Myron <josephonyekachi77@gmail.com>
2022-11-13 20:15:59 -0800

    updated

--- search.php
+++ search.php
@@ -9,9 +9,9 @@ $posts = [];
 $post_title = 'Recent Post';
 
 if (isset($_POST['search-term'])) {
-    $post_title = 'You searched for '. $_POST['search-term'];
+    $post_title = 'You searched for ' . $_POST['search-term'];
     $posts = searchPost($_POST['search-term']);
-}else{
+} else {
     $posts = $posts = getPublishedPost();
 }
 
@@ -20,65 +20,65 @@ if (isset($_POST['search-term'])) {
 
 <div class="content1" style="top: 180px;">
 
-<div class="content" >
+    <div class="content">
 
-    <?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
+        <?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
 
-    <section>
-        <!-------------------------------- Left side ------------------------------------>
-        <div class="left">
-            <!-------------------------------- Music section ------------------------------------>
-            <div class="header_line">
-                <div class="header_title">
-                    <p><span>Search</span></p>
-                </div>
-            </div>
-
-            <p style="font-size:20px; margin-bottom: 1em; font-weight:bold;"><span><?php echo $post_title ?></span></p>
-
-            <div class="Music">
-
-                <?php foreach ($posts as $key => $post) : ?>
-
-                    <div class="card">
-                        <div class="img_sec">
-
-                            <div class="topic">
-                                <p><a href="#"><?php echo $post['topic_id'] ?></a></p>
-                            </div>
-                            <a href="#"><img src="<?php echo BASE_URL . '/assets/images/' . $post['image'] ?>" alt="<?php echo $post['title'] ?>"></a>
-                        </div>
-                        <div class="title_sec">
-                            <a href="#">
-                                <h2 class="title"><?php echo $post['title'] ?></h2>
-                            </a>
-
-                            <a href="#">
-                                <p class="post_content">
-                                    <?php echo substr($post['body'], 0, 120).'...'?>
-                                </p>
-                            </a>
-                            <small>BY <a href="#"><span style="color:red; margin-right: 10px;"><?php echo $post['username'] ?></span></a><?php echo date('F j, Y', strtotime($post['created_at'])) ?></small>
-                        </div>
+        <section>
+            <!-------------------------------- Left side ------------------------------------>
+            <div class="left">
+                <!-------------------------------- Music section ------------------------------------>
+                <div class="header_line">
+                    <div class="header_title">
+                        <p><span>Search</span></p>
                     </div>
+                </div>
 
-                <?php endforeach; ?>
+                <p style="font-size:20px; margin-bottom: 1em; font-weight:bold;"><span><?php echo $post_title ?></span></p>
+
+                <div class="Music">
+
+                    <?php foreach ($posts as $key => $post) : ?>
+
+                        <div class="card">
+                            <div class="img_sec">
+
+                                <div class="topic">
+                                    <p><a href="#"><?php echo $post['topic_id'] ?></a></p>
+                                </div>
+                                <a href="#"><img src="<?php echo BASE_URL . '/assets/images/' . $post['image'] ?>" alt="<?php echo $post['title'] ?>"></a>
+                            </div>
+                            <div class="title_sec">
+                                <a href="#">
+                                    <h2 class="title"><?php echo $post['title'] ?></h2>
+                                </a>
+
+                                <a href="#">
+                                    <p class="post_content">
+                                        <?php echo substr($post['body'], 0, 120) . '...' ?>
+                                    </p>
+                                </a>
+                                <small>BY <a href="#"><span style="color:red; margin-right: 10px;"><?php echo $post['username'] ?></span></a><?php echo date('F j, Y', strtotime($post['created_at'])) ?></small>
+                            </div>
+                        </div>
+
+                    <?php endforeach; ?>
+
+
+                </div>
+                <!-------------------------------- Video Section ------------------------------------>
 
 
             </div>
-            <!-------------------------------- Video Section ------------------------------------>
-            
+            <!-------------------------------- Right side ------------------------------------>
+            <div class="right">
+                <?php include("app/includes/side.php") ?>
 
-        </div>
-        <!-------------------------------- Right side ------------------------------------>
-        <div class="right">
-            <?php include("app/includes/side.php") ?>
+            </div>
+        </section>
 
-        </div>
-    </section>
+    </div>
 
-</div>
-
-<?php include(ROOT_PATH . "/app/includes/footer.php") ?>
+    <?php include(ROOT_PATH . "/app/includes/footer.php") ?>
 
 </div>
